a1d3afff666db0f9750f1234191143b42d2c904c
mvr <matthewvonrocketstein@gmail-dot-com>
2015-06-13 18:37:49 +0000

    add default for /home/vagrant when it's not found (needed for jenkins builds, etc

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,7 @@
 """ tunl/tests/__init__
 """
+import os
+import warnings, tempfile
+if not os.environ.get('HOME'):
+    warnings.warn('$HOME is not set!  using /tmp')
+    os.environ.get('HOME', tempfile.gettempdir())
